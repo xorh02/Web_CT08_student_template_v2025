9207cee76aa0ecbe8fc79ac10696530736c6bd2d
xorh02 <xavierong02@gmail.com>
2026-07-26 03:23:14 +0000

    Jul 26, 2026, 3:23 AM

--- geometrydash/geometrydash_advanced.js
+++ geometrydash/geometrydash_advanced.js
@@ -1,1 +1,20 @@
   // write your codes here
+let box,cube,bg;
+
+function preload(){
+ bg = loadImage("assets/geobg.png");
+  cube = loadImage("assets/cube.png");
+}
+
+function setup(){
+  new Canvas(700,600);
+  world.gravity.y = 32;
+  box = new Sprite(50, height, 50, 50);
+  box.img = cube;
+  box.friction = 0;
+  box.bounciness = 0;
+  box.collider = "none";
+
+  startCoordinates = [50, height - box.height / 2];
+  
+}
