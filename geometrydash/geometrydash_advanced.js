0fa173236b1110a5dde30f782e48638e09e00c8c
xorh02 <xavierong02@gmail.com>
2026-07-26 04:15:13 +0000

    Jul 26, 2026, 4:15 AM

--- geometrydash/geometrydash_advanced.js
+++ geometrydash/geometrydash_advanced.js
@@ -1,10 +1,12 @@
-function draw()
+function draw(){
 
 for (let orb of orbs) {
       if (box.colliding(orb)) {
         orb.visible = false;
         orb.collider = "none";
-        box.vel.y = -5;
+        box.vel.y = -10;
         jumpChance = MAX_JUMP;
       }
     }
+
+  }
